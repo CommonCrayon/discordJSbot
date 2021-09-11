9ff25f754f76f9c53384d4f3c889ccaa91648d6d
CommonCrayon <49395387+CommonCrayon@users.noreply.github.com>
2021-09-11 18:37:07 +1000

    Buttons Work! but do spit out random errors

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,35 +1,66 @@
 const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 
-const yesEntry = ['-'];
-const maybeEntry = ['-'];
-const noEntry = ['-'];
-var x = 0;
-var y = 0;
-var z = 0;
+const yesEntry = [];
+const maybeEntry = [];
+const noEntry = [];
+var x = 0;	// Counter For yesEntry
+var y = 0;	// Counter For maybeEntry
+var z = 0;	// Counter For noEntry
+
 
 module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
-		const userid = (`${interaction.user}`);
 
-		console.log("Outside" + userid);
-
+		user = (`${interaction.user}`)	// Captures user's name
 
 		const collector = interaction.channel.createMessageComponentCollector();
 
         collector.on('collect', async i => {
-			console.log("Inside" + userid);
 
             if (i.customId.includes("yes") ) {
 				await i.deferUpdate();
-				yesEntry.push(userid);
-				theRemovedElement == 1
-				var theRemovedElement = yesEntry.shift()
 
+				yesEntry.push(user);
 
-				let mainEmbed = createEmbed(yesEntry, maybeEntry, noEntry); 
+				//NEEDS TO BE A FUNCTION================================
+				// For Yes
+				if (yesEntry.length == 0){
+					yesString = "Empty";
+				}
+				else {
+					yesString = "";
+					for (var l = 0; l < yesEntry.length; l++) {
+						yesString = (yesString + yesEntry[l] + '\n');
+					}
+				}
+
+				// For Maybe
+				if (maybeEntry.length == 0){
+					maybeString = "Empty";
+				}
+				else {
+					maybeString = "";
+					for (var l = 0; l < maybeEntry.length; l++) {
+						maybeString = (maybeString + maybeEntry[l] + '\n');
+					}
+				}
+
+				// For No
+				if (noEntry.length == 0){
+					noString = "Empty";
+				}
+				else {
+					noString = "";
+					for (var l = 0; l < noEntry.length; l++) {
+						noString = (noString + noEntry[l] + '\n');
+					}
+				}
+				//NEEDS TO BE A FUNCTION================================
+
+				let mainEmbed = createEmbed(yesString, maybeString, noString); 
 				let buttons = createButton(); 
 				
 				await i.editReply({content: '@10-man', 
@@ -40,12 +71,45 @@ module.exports = {
 
             else if (i.customId.includes("maybe") ) {
 				await i.deferUpdate();
-				maybeEntry.push(userid);
-				theRemovedElement == 1
-				var theRemovedElement = maybeEntry.shift()
 
+				maybeEntry.push(user);
 
-				let mainEmbed = createEmbed(yesEntry, maybeEntry, noEntry); 
+				//NEEDS TO BE A FUNCTION================================
+				// For Yes
+				if (yesEntry.length == 0){
+					yesString = "Empty";
+				}
+				else {
+					yesString = "";
+					for (var l = 0; l < yesEntry.length; l++) {
+						yesString = (yesString + yesEntry[l] + '\n');
+					}
+				}
+
+				// For Maybe
+				if (maybeEntry.length == 0){
+					maybeString = "Empty";
+				}
+				else {
+					maybeString = "";
+					for (var l = 0; l < maybeEntry.length; l++) {
+						maybeString = (maybeString + maybeEntry[l] + '\n');
+					}
+				}
+
+				// For No
+				if (noEntry.length == 0){
+					noString = "Empty";
+				}
+				else {
+					noString = "";
+					for (var l = 0; l < noEntry.length; l++) {
+						noString = (noString + noEntry[l] + '\n');
+					}
+				}
+				//NEEDS TO BE A FUNCTION================================
+
+				let mainEmbed = createEmbed(yesString, maybeString, noString); 
 				let buttons = createButton(); 
 				
 				await i.editReply({content: '@10-man', 
@@ -56,12 +120,45 @@ module.exports = {
 
             else if (i.customId.includes("no") ) {
 				await i.deferUpdate();
-				noEntry.push(userid);
-				theRemovedElement == 1
-				var theRemovedElement = noEntry.shift()
 
+				noEntry.push(user);
 
-				let mainEmbed = createEmbed(yesEntry, maybeEntry, noEntry); 
+				//NEEDS TO BE A FUNCTION================================
+				// For Yes
+				if (yesEntry.length == 0){
+					yesString = "Empty";
+				}
+				else {
+					yesString = "";
+					for (var l = 0; l < yesEntry.length; l++) {
+						yesString = (yesString + yesEntry[l] + '\n');
+					}
+				}
+
+				// For Maybe
+				if (maybeEntry.length == 0){
+					maybeString = "Empty";
+				}
+				else {
+					maybeString = "";
+					for (var l = 0; l < maybeEntry.length; l++) {
+						maybeString = (maybeString + maybeEntry[l] + '\n');
+					}
+				}
+
+				// For No
+				if (noEntry.length == 0){
+					noString = "Empty";
+				}
+				else {
+					noString = "";
+					for (var l = 0; l < noEntry.length; l++) {
+						noString = (noString + noEntry[l] + '\n');
+					}
+				}
+				//NEEDS TO BE A FUNCTION================================
+
+				let mainEmbed = createEmbed(yesString, maybeString, noString); 
 				let buttons = createButton(); 
 				
 				await i.editReply({content: '@10-man', 
@@ -78,7 +175,7 @@ module.exports = {
 
 function createEmbed(yesEntry, maybeEntry, noEntry) {
 	const mainEmbed = new MessageEmbed()
-	.setThumbnail('https://imgur.com/vUG7MDU.png')
+	//.setThumbnail('https://imgur.com/vUG7MDU.png')
 	.setColor('0xFF6F00')
 	.setTitle('10 Man')
 	.setURL('https://10man.commoncrayon.com/')
@@ -86,11 +183,11 @@ function createEmbed(yesEntry, maybeEntry, noEntry) {
 	.addFields(
 		{ name: 'Time:', value: '20:30' },
 		{ name: 'Countdown:', value: 'number' },
-		{ name: 'Yes: (' + yesEntry.length + ')', value: ' ' + yesEntry, inline: true},
-		{ name: 'Maybe: (' + maybeEntry.length + ')', value: ' ' + maybeEntry, inline: true },
-		{ name: 'No: (' + noEntry.length + ')', value: ' ' + noEntry, inline: true },
+		{ name: 'Yes: ', value: yesEntry, inline: true},
+		{ name: 'Maybe: ', value: maybeEntry, inline: true },
+		{ name: 'No: ', value: noEntry, inline: true },
 		)
-	.setFooter('connect crayon.csgo.fr:27015; password fun', 'https://i.imgur.com/nuEpvJd.png');
+	.setFooter('Server IP: connect crayon.csgo.fr:27015; password fun', 'https://i.imgur.com/nuEpvJd.png');
 	return mainEmbed;
 }
 
